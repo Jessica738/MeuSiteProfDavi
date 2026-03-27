5a482015800e134300de42cba5fecd44d363f90d
Jessica738 <00001103011480sp@al.educacao.sp.gov.br>
2026-03-27 08:40:25 -0300

    atualização 4
    Adicionei mais um filme e mais uma serie

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ let filmes = [
     titulo: "Meninas não choram",
     imagem: "Imagens/MeninasNãoChoram.png",
     descricao: "acompanha Pipa, uma adolescente de 16 anos apaixonada por futebol e craque do time escolar. Sua vida muda drasticamente ao ser diagnosticada com leucemia, enfrentando internações e o tratamento."
+    },
+
+    {
+        titulo: "Salve Rosa",
+        imagem: "Imagens/SalveRosa.png",
+        descricao: "acompanha Rosa, uma influenciadora mirim de 13 anos com milhões de seguidores, cuja vida 'perfeita' nas redes esconde uma rotina sufocante de controle obsessivo e manipulação por sua mãe, Dora (Karine Teles), explorando temas como superexposição infantil, relações tóxicas e fama precoce."
     }
     
     ];
@@ -13,12 +19,19 @@ let filmes = [
     let series = [
     
     {
-    titulo: "Stranger Things",
-    imagem: "Imagens/StrangerThings.png",
-    descricao: "Um grupo de crianças enfrenta forças sobrenaturais e experimentos secretos em uma pequena cidade."
-    }
+    titulo: "Garota de fora",
+    imagem: "Imagens/GarotaDeFora.png",
+    descricao: "Nanno, uma estudante enigmática e imortal, transfere-se para diferentes escolas para expor e punir mentiras, hipocrisias e crimes de alunos e professores."
 
+    },
+
+    {
+        titulo: "Stranger Things",
+        imagem: "Imagens/StrangerThings.png",
+        descricao: "Um grupo de crianças enfrenta forças sobrenaturais e experimentos secretos em uma pequena cidade."
     
+        }
+
     ];
     
     function mostrar(){
