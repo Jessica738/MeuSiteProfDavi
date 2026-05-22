57b7da7a26fd89fa5697ac4f5d0bd7cba1fa8c87
Jessica738 <00001103011480sp@al.educacao.sp.gov.br>
2026-05-22 09:02:58 -0300

    Arquivos do Prof

---
+++ filmes.js
@@ -0,0 +1,27 @@
+// filmes.js
+
+export const filmes = [
+    {
+        titulo: "Interestelar",
+        imagem: "Imagens/Interstellar_Filme.png",
+        descricao: "Um grupo de astronautas viaja através de um buraco de minhoca em busca de um novo lar para a humanidade."
+    },
+    {
+        titulo: "Batman",
+        imagem: "Imagens/batima.jpg",
+        descricao: "O herói de Gotham enfrenta novos desafios."
+    }
+];
+
+export const series = [
+    {
+        titulo: "Stranger Things",
+        imagem: "Imagens/StrangerThings.png",
+        descricao: "Um grupo de crianças enfrenta forças sobrenaturais."
+    },
+    {
+        titulo: "Dark",
+        imagem: "Imagens/dark.jpg",
+        descricao: "Mistérios envolvendo viagens no tempo."
+    }
+];
