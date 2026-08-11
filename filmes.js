b9bf528bc7270ede016cc4c9ce457abd64b95752
Jessica738 <00001103011480sp@al.educacao.sp.gov.br>
2026-08-11 08:25:56 -0300

    MUDEI MIL E UMA COISA
    peguei os novos codigos do davi

--- filmes.js
+++ filmes.js
@@ -2,19 +2,46 @@
 
 export const filmes = [
     {
+        id: "interestelar",
         titulo: "Interestelar",
-        imagem: "Imagens/Interstellar_Filme.png",
-        descricao: "Um grupo de astronautas viaja através de um buraco de minhoca em busca de um novo lar para a humanidade."
+        imagem: "Imagens/interstellar_Filme.png",
+        descricao: "Um grupo de astronautas viaja através de um buraco de minhoca em busca de um novo lar para a humanidade.",
+        trailer: "i6avfCqKcQo?si=c67bqEGle7cgphln"
     },
+
     {
-        titulo: "Batman",
+        titulo: "Lego Batman",
         imagem: "Imagens/batima.jpg",
-        descricao: "O herói de Gotham enfrenta novos desafios."
+        descricao: "Em Batman: O Cavaleiro das Trevas, a paz é ameaçada pelo Coringa, um anarquista que instaura o caos.",
+        trailer: "wGxYas0-QPY?si=O2S-tlitxGkHCNN1"
     },
+
     {
-        titulo: "Como magica",
-        imagem: "Imagens/images.jpg",
-        descricao: "O longa acompanha Ollie, uma pequena criatura da floresta, e Ivy, um pássaro majestoso, que são inimigos naturais no reino animal chamado Vale"
+        titulo: "Ong Bak",
+        imagem: "Imagens/ong bak.jpg",
+        descricao: "Ong-Bak: Guerreiro Sagrado, acompanha Ting (Tony Jaa), um jovem guerreiro de uma pacata vila tailandesa.",
+        trailer: "GQ5qcPsCP9A?si=ZqKFcTAlrzLUj7zC"
+    },
+
+    {
+        titulo: "Kung-Fusão",
+        imagem: "Imagens/Kung-Fusão.jpg",
+        descricao: "A história acompanha Sing, um malandro trapalhão que tenta entrar para a temida Gangue do Machado.",
+        trailer: "8UwNoy2KRS4?si=2L5o29EOmtyOIJXy"
+    },
+
+    {
+        titulo: "Homem Aranha",
+        imagem: "Imagens/Homem Aranha.jpg",
+        descricao: "O jovem Peter Parker que, após ser picado por uma aranha, ganha superpoderes como força, agilidade e a capacidade de escalar paredes.",
+        trailer: ""
+    },
+
+    {
+        titulo: "Capitão América: Guerra Civil",
+        imagem: "Imagens/Capitão América.jpg",
+        descricao: "Após danos colaterais em missões dos Vingadores, a ONU propõe o Tratado de Sokovia, um acordo para regulamentar e controlar a equipe.",
+        trailer: ""
     }
 ];
 
@@ -22,17 +49,37 @@ export const series = [
     {
         titulo: "Stranger Things",
         imagem: "Imagens/StrangerThings.png",
-        descricao: "Um grupo de crianças enfrenta forças sobrenaturais."
+        descricao: "Um grupo de crianças enfrenta forças sobrenaturais.",
+        trailer: "ID_YOUTUBE"
     },
     {
         titulo: "Dark",
         imagem: "Imagens/dark.jpg",
-        descricao: "Mistérios envolvendo viagens no tempo."
+        descricao: "Dark é uma série focada em viagens no tempo e multiversos.",
+        trailer: "ID_YOUTUBE"
     },
     {
-        titulo: "Icarly",
-        imagem: "Imagens/icarly.jpg",
-        descricao: "iCarly é uma série de comédia americana criada por Dan Schneider para a Nickelodeon, exibida originalmente de 2007 a 2012. A trama acompanha Carly Shay (Miranda Cosgrove), seu irmão Spencer e seus amigos Sam e Freddie, que criam um programa de sucesso na internet."
+        titulo: "Breaking Bad",
+        imagem: "Imagens/Breaking Bad.jpg",
+        descricao: " A série acompanha Walter White e a sua transformação de um homem comum no chefão do tráfico.",
+        trailer: "ID_YOUTUBE"
+    },
+    {
+        titulo: "Peaky Blinders",
+        imagem: "Imagens/Peaky Blinders.jpg",
+        descricao: "Peaky Blinders acompanha Thomas Shelby e sua gangue em Birmingham após a Primeira Guerra Mundial.",
+        trailer: "ID_YOUTUBE"
+    },
+    {
+        titulo: "Round 6",
+        imagem: "Imagens/Round 6.jpg",
+        descricao: "Centenas de jogadores falidos aceitam um estranho convite para um jogo de sobrevivência. Onde um prêmio milionário aguarda.",
+        trailer: "ID_YOUTUBE"
+    },
+    {
+        titulo: "Avatar A lenda de Aang",
+        imagem: "Imagens/Avatar A lenda de Aang.jpg",
+        descricao: "Avatar: A Lenda de Aang acompanha Aang, um menino de 12 anos que acorda após cem anos congelado em um iceberg.",
+        trailer: "ID_YOUTUBE"
     }
 ];
-
