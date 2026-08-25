fd55e595bad6d787c35fa2505ac0c819425d4259
Jessica738 <00001103011480sp@al.educacao.sp.gov.br>
2026-08-25 08:26:29 -0300

    duração grrr

--- filmes.js
+++ filmes.js
@@ -6,7 +6,10 @@ export const filmes = [
         titulo: "Interestelar",
         imagem: "Imagens/interstellar_Filme.png",
         descricao: "Um grupo de astronautas viaja através de um buraco de minhoca em busca de um novo lar para a humanidade.",
-        trailer: "i6avfCqKcQo?si=c67bqEGle7cgphln"
+        trailer: "i6avfCqKcQo?si=c67bqEGle7cgphln",
+        duracao: "2h 49min",
+        genero: "Ficção científica, Drama",
+        classificacao: "10 anos"
     },
 
     {
@@ -14,7 +17,10 @@ export const filmes = [
         titulo: "Lego Batman",
         imagem: "Imagens/batima.jpg",
         descricao: "Em Batman: O Cavaleiro das Trevas, a paz é ameaçada pelo Coringa, um anarquista que instaura o caos.",
-        trailer: "wGxYas0-QPY?si=O2S-tlitxGkHCNN1"
+        trailer: "wGxYas0-QPY?si=O2S-tlitxGkHCNN1",
+        duracao: "1h 44min",
+        genero: "Animação, Ação, Comédia",
+        classificacao: "Livre"
     },
 
     {
@@ -22,7 +28,10 @@ export const filmes = [
         titulo: "Ong Bak",
         imagem: "Imagens/ong bak.jpg",
         descricao: "Ong-Bak: Guerreiro Sagrado, acompanha Ting (Tony Jaa), um jovem guerreiro de uma pacata vila tailandesa.",
-        trailer: "GQ5qcPsCP9A?si=ZqKFcTAlrzLUj7zC"
+        trailer: "GQ5qcPsCP9A?si=ZqKFcTAlrzLUj7zC",
+        duracao: "1h 45min",
+        genero: "Ação, Artes marciais",
+        classificacao: "16 anos"
     },
 
     {
@@ -30,7 +39,10 @@ export const filmes = [
         titulo: "Kung-Fusão",
         imagem: "Imagens/Kung-Fusão.jpg",
         descricao: "A história acompanha Sing, um malandro trapalhão que tenta entrar para a temida Gangue do Machado.",
-        trailer: "8UwNoy2KRS4?si=2L5o29EOmtyOIJXy"
+        trailer: "8UwNoy2KRS4?si=2L5o29EOmtyOIJXy",
+        duracao: "1h 39min",
+        genero: "Ação, Comédia",
+        classificacao: "14 anos"
     },
 
     {
@@ -38,7 +50,10 @@ export const filmes = [
         titulo: "Homem Aranha",
         imagem: "Imagens/Homem Aranha.jpg",
         descricao: "O jovem Peter Parker que, após ser picado por uma aranha, ganha superpoderes como força, agilidade e a capacidade de escalar paredes.",
-        trailer: "PlulyWs1kS4?si=l3W3HT2aRGunhWy_"
+        trailer: "PlulyWs1kS4?si=l3W3HT2aRGunhWy_",
+        duracao: "2h 1min",
+        genero: "Ação, Aventura, Fantasia",
+        classificacao: "10 anos"
     },
 
     {
@@ -46,7 +61,10 @@ export const filmes = [
         titulo: "Capitão América: Guerra Civil",
         imagem: "Imagens/Capitão América.jpg",
         descricao: "Após danos colaterais em missões dos Vingadores, a ONU propõe o Tratado de Sokovia, um acordo para regulamentar e controlar a equipe.",
-        trailer: "FRLYZn5PGuA?si=KX1nbXIosmYT5iIX"
+        trailer: "FRLYZn5PGuA?si=KX1nbXIosmYT5iIX",
+        duracao: "2h 27min",
+        genero: "Ação, Aventura, Ficção científica",
+        classificacao: "12 anos"
     }
 ];
 
@@ -56,41 +74,64 @@ export const series = [
         titulo: "Stranger Things",
         imagem: "Imagens/StrangerThings.png",
         descricao: "Um grupo de crianças enfrenta forças sobrenaturais.",
-        trailer: "RMmGQNNl164?si=BlGNLayRu-JaRrxs"
+        trailer: "RMmGQNNl164?si=BlGNLayRu-JaRrxs",
+        duracao: "50min - 1h 20min por episódio",
+        genero: "Ficção científica, Terror, Drama",
+        classificacao: "16 anos"
     },
+
     {
         id: "Dark",
         titulo: "Dark",
         imagem: "Imagens/dark.jpg",
         descricao: "Dark é uma série focada em viagens no tempo e multiversos.",
-        trailer: "JCCssUOtn2E?si=xFIqL-NQyTD7bVwq"
+        trailer: "JCCssUOtn2E?si=xFIqL-NQyTD7bVwq",
+        duracao: "45min - 1h por episódio",
+        genero: "Ficção científica, Mistério, Drama",
+        classificacao: "16 anos"
     },
+
     {
         id: "Breaking Bad",
         titulo: "Breaking Bad",
         imagem: "Imagens/Breaking Bad.jpg",
-        descricao: " A série acompanha Walter White e a sua transformação de um homem comum no chefão do tráfico.",
-        trailer: "2gTC4uWP3_Y?si=F9JgES6_c4SQtkgv"
+        descricao: "A série acompanha Walter White e a sua transformação de um homem comum no chefão do tráfico.",
+        trailer: "2gTC4uWP3_Y?si=F9JgES6_c4SQtkgv",
+        duracao: "43min - 60min por episódio",
+        genero: "Drama, Crime, Suspense",
+        classificacao: "18 anos"
     },
+
     {
         id: "Peaky Blinders",
         titulo: "Peaky Blinders",
         imagem: "Imagens/Peaky Blinders.jpg",
         descricao: "Peaky Blinders acompanha Thomas Shelby e sua gangue em Birmingham após a Primeira Guerra Mundial.",
-        trailer: "NfQ-dwkuMx8?si=vmruPEgthKUgGLTw"
+        trailer: "NfQ-dwkuMx8?si=vmruPEgthKUgGLTw",
+        duracao: "50min - 1h por episódio",
+        genero: "Drama, Crime, História",
+        classificacao: "16 anos"
     },
+
     {
         id: "Round 6",
         titulo: "Round 6",
         imagem: "Imagens/Round 6.jpg",
         descricao: "Centenas de jogadores falidos aceitam um estranho convite para um jogo de sobrevivência. Onde um prêmio milionário aguarda.",
-        trailer: "CmRR-0XAttY?si=eol0Hqpm1Y2USAmD"
+        trailer: "CmRR-0XAttY?si=eol0Hqpm1Y2USAmD",
+        duracao: "32min - 1h 20min por episódio",
+        genero: "Drama, Suspense, Sobrevivência",
+        classificacao: "16 anos"
     },
+
     {
         id: "Avatar A lenda de Aang",
         titulo: "Avatar A lenda de Aang",
         imagem: "Imagens/Avatar A lenda de Aang.jpg",
         descricao: "Avatar: A Lenda de Aang acompanha Aang, um menino de 12 anos que acorda após cem anos congelado em um iceberg.",
-        trailer: "ooVvH2IYz0w?si=xve8SS2PMZh_mfDp"
+        trailer: "ooVvH2IYz0w?si=xve8SS2PMZh_mfDp",
+        duracao: "23min por episódio",
+        genero: "Animação, Aventura, Fantasia",
+        classificacao: "Livre"
     }
 ];
